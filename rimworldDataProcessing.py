7ba3a444d207841430ac1c71582a608645fcf33b
panerotism <panerotism@protonmail.com>
2025-05-29 02:06:42 +0200

    my god it works it really does

--- rimworldDataProcessing.py
+++ rimworldDataProcessing.py
@@ -1,67 +1,59 @@
-import os
 import xml.etree.ElementTree as ET
 import csv
+from pathlib import Path
 
 # -------- CONFIG --------
 RIMWORLD_FOLDER = "C:\\Applications\\RimWorld"
-DEFPATHTYPE_1 = "1.5\\Defs"
-DEFPATHTYPE_2 = "v1.5\\Defs"
-DEFPATHTYPE_3 = "Defs"
 # --------
 
 
 # Classes
 class plant:
-    name = "undefinedPlant"
-    plantHarvestYield = 0
+    def __init__(self):
+        self.name = "undefinedPlant"
+        self.harvestYield = 0
 
 
 # Process defs
 def extractDef(defFilePath, sheets):
-    defName = os.path.basename(defFilePath)
-    print("test")
-    # Plants
-    # if root.thingDef.plant --> If this exists then it is a plant!
-    defSearchKey = "Plants_"
-    if defName.find(defSearchKey) != -1:
+    sheetName = "undefined"
+    entry = "undefined"
+
+    try:
         tree = ET.parse(defFilePath)
         root = tree.getroot()
-        print(root)
+        for element1 in root:  # ThingDef
+            for element2 in element1:  # e.g label
+                if element2.tag == "plant":
+                    sheetName = "plants"
+    except Exception:
+        pass
+
+    match sheetName:
+        case "plants":
+            for element1 in root:  # ThingDef
+                entry = plant()
+                for element2 in element1:  # e.g label
+                    match element2.tag:
+                        case "label":
+                            entry.name = element2.text
+                        case "plant":
+                            for element3 in element2:
+                                match element3.tag:
+                                    case "harvestYield":
+                                        entry.harvestYield = element3.text
+                sheets[sheetName].append(entry)
 
 
 # Returns a list with every def path
-def getDefPaths():
-    defFilePathList = []
-    defPathTypes = [
-        RIMWORLD_FOLDER + "\\Data"
-        DEFPATHTYPE_1,
-        DEFPATHTYPE_2,
-        DEFPATHTYPE_3]
-
-    for defPathType in defPathTypes:
-        defsFolderPath = os.path.join(RIMWORLD_FOLDER, defPathType)
-        print(os.path.exists(defsFolderPath))
-        if os.path.exists(defsFolderPath):
-            for defFolderPath in os.listdir(defsFolderPath):
-                for defSubfolderPath in os.listdir(defFolderPath):
-                    defFilePath = os.path.join(defFolderPath, defSubfolderPath)
-                    if os.path.isdir(defFilePath):
-                        for defFile in os.listdir(defFilePath):
-                            defPath = os.path.join(defFilePath, defFile)
-                            defFilePathList.append(defPath)
-
+def getXMLFilePaths():
     # Returns the filelist
-    return defFilePathList
-
-
-# Writes CSVs
-def write_csv(sheets):
-    for sheet in sheets:
-        with open(sheet + ".csv", "w", newline="", encoding="utf-8") as csvf:
-            writer = csv.writer(csvf)
-            for rowNumber in range(len(sheet)):
-                row = sheet[rowNumber]
-                writer.writerows(row)
+    XMLFilePaths = []
+    excludedKeywords = ["1.0", "1.1", "1.2", "1.3", "1.4"]
+    for XMLFile in Path(RIMWORLD_FOLDER).rglob("**/*.xml"):
+        if not any(keyword in str(XMLFile) for keyword in excludedKeywords):
+            XMLFilePaths.append(XMLFile)
+    return XMLFilePaths
 
 
 # Defines variables for each sheet
@@ -78,12 +70,17 @@ sheets = {
 # Code execution starts here
 if __name__ == "__main__":
     # Get a list with every def path
-    defFilePathList = getDefPaths()
+    XMLFilePathList = getXMLFilePaths()
 
     # Extract every def
-    for defFilePath in defFilePathList:
-        extractDef(defFilePath, sheets)
+    for XMLFilePath in XMLFilePathList:
+        extractDef(XMLFilePath, sheets)
 
     # Write the CSVs
-    for sheet in sheets:
-        write_csv(sheet)
+    for sheetName in sheets.keys():
+        sheet = sheets[sheetName]
+        with open(sheetName + ".csv", "w", newline="", encoding="utf-8") as csvf:
+            writer = csv.writer(csvf)
+            writer.writerow(["Name", "Yield"])
+            for entry in sheet:
+                writer.writerow([entry.name, entry.harvestYield])
